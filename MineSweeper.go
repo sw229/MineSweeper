2a336ed6eaed36f32ba77f1fe8789132ee317a24
sw229 <155364398+sw229@users.noreply.github.com>
2024-06-07 20:30:02 +0300

    Add files via upload

--- MineSweeper.go
+++ MineSweeper.go
@@ -2,84 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
 )
 
-func newField(mineNum int, field [][]int) [][]int {
-
-	return field
-}
-
-func markCell(i, j int, visibleField [][]string) [][]string {
-	visibleField[i][j] = "P"
-	return visibleField
-}
-
-func openCell(i, j int, field [][]int, visibleField [][]string) (bool, [][]string) {
-	if field[i][j] == -1 {
-		return true, visibleField
-	} else if field[i][j] != 0 {
-		visibleField[i][j] = strconv.Itoa(field[i][j])
-		return false, visibleField
-	} else if visibleField[i][j] != "_" {
-		visibleField[i][j] = "_"
-
-		if i > 0 && j > 0 {
-			_, visibleField = openCell(i-1, j-1, field, visibleField)
-		}
-		if i > 0 {
-			_, visibleField = openCell(i-1, j, field, visibleField)
-		}
-		if i > 0 && j < len(field[0])-1 {
-			_, visibleField = openCell(i-1, j+1, field, visibleField)
-		}
-		if j > 0 {
-			_, visibleField = openCell(i, j-1, field, visibleField)
-		}
-		if j < len(field[0])-1 {
-			_, visibleField = openCell(i, j+1, field, visibleField)
-		}
-		if i < len(field)-1 && j > 0 {
-			_, visibleField = openCell(i+1, j-1, field, visibleField)
-		}
-		if i < len(field)-1 {
-			_, visibleField = openCell(i+1, j, field, visibleField)
-		}
-		if i < len(field)-1 && j < len(field[0])-1 {
-			_, visibleField = openCell(i+1, j+1, field, visibleField)
-		}
-	}
-
-	return false, visibleField
-}
-
-func main() {
-	//var gameOver bool
-	size_h := 10
-	size_v := 10
+func newFields(mineNum, size_v, size_h int) ([][]int, [][]string) {
 	field := make([][]int, size_v)
 	for i := range field {
 		field[i] = make([]int, size_h)
 	}
-	field[1][1] = -1
-	/*
-	   for i := range(field){
-	       for j := range(field[0]){
-	           if field[i][j] != -1 {
-	               for k := i - 1; k <= i + 1; k++{
-	                   for l := j - 1; l <= j + 1; j++{
-	                       if k < 0 || k > size_v - 1 || l < 0 || l > size_h - 1 {
-	                           continue
-	                       } else if field[k][l] == -1 {
-	                           field[i][j] += 1
-	                       }
-	                   }
-	               }
-	           }
-	       }
-	   }
-	*/
 
+	for i := 0; i < mineNum; i++ {
+		x := rand.Intn(len(field))
+		y := rand.Intn(len(field))
+		if field[y][x] == -1 {
+			i -= 1
+		} else {
+			field[y][x] = -1
+		}
+	}
 	for i := range field {
 		for j := range field[0] {
 			if field[i][j] != -1 {
@@ -116,8 +57,72 @@ func main() {
 		visibleField[i] = make([]string, size_h)
 	}
 
-	//visibleField = markCell(1, 1, visibleField)
-	_, visibleField = openCell(9, 9, field, visibleField)
+	return field, visibleField
+}
+
+func markCell(i, j int, visibleField [][]string) [][]string {
+	visibleField[i][j] = "P"
+	return visibleField
+}
+
+func openCell(i, j int, field [][]int, visibleField [][]string) (bool, [][]string) {
+	if visibleField[i][j] == "P" {
+	} else if field[i][j] == -1 {
+		return true, visibleField
+	} else if field[i][j] != 0 {
+		visibleField[i][j] = strconv.Itoa(field[i][j])
+		return false, visibleField
+	} else if visibleField[i][j] != "_" {
+		visibleField[i][j] = "_"
+
+		if i > 0 && j > 0 {
+			_, visibleField = openCell(i-1, j-1, field, visibleField)
+		}
+		if i > 0 {
+			_, visibleField = openCell(i-1, j, field, visibleField)
+		}
+		if i > 0 && j < len(field[0])-1 {
+			_, visibleField = openCell(i-1, j+1, field, visibleField)
+		}
+		if j > 0 {
+			_, visibleField = openCell(i, j-1, field, visibleField)
+		}
+		if j < len(field[0])-1 {
+			_, visibleField = openCell(i, j+1, field, visibleField)
+		}
+		if i < len(field)-1 && j > 0 {
+			_, visibleField = openCell(i+1, j-1, field, visibleField)
+		}
+		if i < len(field)-1 {
+			_, visibleField = openCell(i+1, j, field, visibleField)
+		}
+		if i < len(field)-1 && j < len(field[0])-1 {
+			_, visibleField = openCell(i+1, j+1, field, visibleField)
+		}
+	}
+
+	return false, visibleField
+}
+
+func isVictory(field [][]int, visibleField [][]string) bool {
+	for i := range field {
+		for j := range field[0] {
+			if field[i][j] != -1 && visibleField[i][j] != "_" {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	//var gameOver bool
+	size_h := 4
+	size_v := 4
+	mineNum := 2
+	gameOver := false
+
+	field, visibleField := newFields(mineNum, size_v, size_h)
 
 	for i := range field {
 		for j := range field[i] {
@@ -126,13 +131,51 @@ func main() {
 		fmt.Println()
 	}
 
-	fmt.Println("\n\n")
+	fmt.Print("\n\n\n")
 
 	for i := range visibleField {
 		for j := range visibleField[i] {
-			fmt.Printf("%s ", visibleField[i][j])
+			if visibleField[i][j] == "" {
+				fmt.Print("O ")
+			} else {
+				fmt.Printf("%s ", visibleField[i][j])
+			}
 		}
 		fmt.Println()
+	}
+
+	fmt.Print("\n\n\n")
+
+	for gameOver == false {
+		var x, y int
+		fmt.Println("input line")
+		fmt.Scan(&y)
+		fmt.Println("input column")
+		fmt.Scan(&x)
+		gameOver, visibleField = openCell(y, x, field, visibleField)
+		gameOver = isVictory(field, visibleField)
+
+		for i := range field {
+			for j := range field[i] {
+				fmt.Printf("%d ", field[i][j])
+			}
+			fmt.Println()
+		}
+
+		fmt.Print("\n\n\n")
+
+		for i := range visibleField {
+			for j := range visibleField[i] {
+				if visibleField[i][j] == "" {
+					fmt.Print("O ")
+				} else {
+					fmt.Printf("%s ", visibleField[i][j])
+				}
+			}
+			fmt.Println()
+		}
+
+		fmt.Print("\n\n\n")
 	}
 
 }
